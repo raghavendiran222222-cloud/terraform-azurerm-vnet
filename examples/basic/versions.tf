b36971e716d6c70f11f4c24f6d6ae6e935c4c8bc
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-05-07 13:54:16 +0530

    feat: add examples/basic/versions.tf for provider requirements

---
+++ examples/basic/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 2.4.0"
+    }
+  }
+}
